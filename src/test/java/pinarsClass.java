78117f625c8cfb0e09202f6aec95cdf2e5a91f08
pinar3146 <pinaryanar3146@hotmail.com>
2020-10-13 22:40:45 +0200

    added new stuff from pinar

--- src/test/java/pinarsClass.java
+++ src/test/java/pinarsClass.java
@@ -2,7 +2,6 @@ public class pinarsClass {
     public static void main(String[] args) {
         System.out.println("this is a new class");
         //adding more stuff here
-        //before to die OMG
-
+        //add again and again
     }
 }
