88b53c8b48a7b4e4e35b643c7bf3808d67004590
AdemTEN <marrysnm2012@gmal.com>
2020-10-13 22:05:45 +0200

    This is the last changing

--- src/test/java/NoConflictBitte2.java
+++ src/test/java/NoConflictBitte2.java
@@ -2,5 +2,6 @@ public class NoConflictBitte2 {
     public static void main(String[] args) {
         System.out.println("I added one more no conflict class");
         //Iadded some comment to second class
+        //This is the last changing
     }
 }
