af4b9fd879400b97c74ec9bf6f5a31edb3ff21ed
AdemTEN <marrysnm2012@gmal.com>
2020-10-13 22:05:00 +0200

    Iadded some comment to second class

--- src/test/java/NoConflictBitte2.java
+++ src/test/java/NoConflictBitte2.java
@@ -1,5 +1,6 @@
 public class NoConflictBitte2 {
     public static void main(String[] args) {
         System.out.println("I added one more no conflict class");
+        //Iadded some comment to second class
     }
 }
