0c2869e489a8e4ea8ab341a6e740d6a5003ee845
Mustafa-Gol <65710512+Mustafa-Gol@users.noreply.github.com>
2020-10-13 21:43:45 +0200

    new Addition by GOL

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("merhaba world");
+        System.out.println("This message is added by de Gol");
     }
 }
