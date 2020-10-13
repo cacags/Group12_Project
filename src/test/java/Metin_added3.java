c03d645fcf7c63c552c645e21d4c5f7c2fc41aff
aMc-69 <CT.AMC.69@gmail.com>
2020-10-13 22:21:37 +0200

    after MetinBranch created

---
+++ src/test/java/Metin_added3.java
@@ -0,0 +1,5 @@
+public class Metin_added3 {
+    public static void main(String[] args) {
+        System.out.println("metin added the 3rd file as java");
+    }
+}
--- src/test/java/isyan.java
+++ src/test/java/isyan.java
@@ -1,1 +1,6 @@
-I am Serkan I want to conflict
+public class isyan {
+    public static void main(String[] args) {
+        System.out.println("I am Serkan I want to conflict");
+    //  I corrected the isyan.java file
+    }
+}
