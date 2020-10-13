54175c1b8dc02687174176ea9f250ebf2b4bec7d
aMc-69 <CT.AMC.69@gmail.com>
2020-10-13 22:37:39 +0200

    after MetinBranch2.java created

---
+++ src/test/java/MetinBranch2.java
@@ -0,0 +1,5 @@
+public class MetinBranch2 {
+    public static void main(String[] args) {
+        System.out.println("Metin made the new branch named MetinBranch2");
+    }
+}
--- src/test/java/isyan.java
+++ src/test/java/isyan.java
@@ -1,1 +1,7 @@
-I am Serkan I want to conflict
+public class isyan {
+    public static void main(String[] args) {
+        System.out.println("I am Serkan I want to conflict");
+ // how do you like this?
+    }
+
+}
