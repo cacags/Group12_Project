59b024200d06817113ea81b62439252d6be29d6b
AdemTEN <marrysnm2012@gmal.com>
2020-10-13 22:03:49 +0200

    i added second no conflict class

--- src/test/java/NoConflictBitte.java
+++ src/test/java/NoConflictBitte.java
@@ -2,5 +2,6 @@ public class NoConflictBitte {
     public static void main(String[] args) {
         System.out.println("please don't create any conlict");
         //Danke
+        //Wie geht es Ihnen?
     }
 }
