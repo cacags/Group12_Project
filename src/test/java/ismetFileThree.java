ebc74a2203f9ec6718b5551464edd98a8036a067
IsmetYigitbasi <ismetyigitbasi@gmail.com>
2020-10-13 22:29:52 +0200

    Added step1

---
+++ src/test/java/ismetFileThree.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ismetFileThree {
+
+    public static void main(String[] args) {
+        // First Step
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int num = scanner.nextInt();
+    }
+}
